a33f53ae8a1136fdf27ac128bc34c3b3d7651d72
AyaanSaleemMalik <Juberbaraut@gmail.com>
2019-11-18 01:05:39 +0000

    oxygen: ril remove ril daemon from init.qcom.rc * fixup wring file name init.class.main.sh to init.class_main.sh

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -79,10 +79,10 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.class.main.sh
+LOCAL_MODULE       := init.class_main.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/init.class.main.sh
+LOCAL_SRC_FILES    := bin/init.class_main.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
